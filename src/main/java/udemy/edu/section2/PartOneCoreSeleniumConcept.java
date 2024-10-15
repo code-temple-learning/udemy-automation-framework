cd963930e033b18472d8d458e2111871b2020624
anthonyhenderson <cuzixsin@gmail.com>
2024-10-15 14:09:04 -0700

    Driver setup

--- src/main/java/udemy/edu/section2/PartOneCoreSeleniumConcept.java
+++ src/main/java/udemy/edu/section2/PartOneCoreSeleniumConcept.java
@@ -1,4 +1,12 @@
 package udemy.edu.section2;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PartOneCoreSeleniumConcept {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+    }
+
 }

89644a0880bcbfeac31297711ce09cf5f770fa6a
anthonyhenderson <cuzixsin@gmail.com>
2024-10-15 13:23:39 -0700

    New method first commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -3,4 +3,7 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("sudo code, YEEEEEEEEE");
     }
+    public void firstCommit() {
+        System.out.println("This is Anthonys first commit");
+    }
 }

c81176cf3a6e402f922fa174ee078670e2e6dedc
anthonyhenderson <cuzixsin@gmail.com>
2024-10-15 13:26:25 -0700

    added statement

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,6 +2,7 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("sudo code, YEEEEEEEEE");
+        System.out.println("new sudo code");
     }
     public void firstCommit() {
         System.out.println("This is Anthonys first commit");

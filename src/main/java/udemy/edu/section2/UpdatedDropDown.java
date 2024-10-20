b66d9061242442c91ea237c5d07bae33258f7b51
anthonyhenderson <cuzixsin@gmail.com>
2024-10-20 12:02:37 -0700

    Used Select webdriver for Static dropdowns

---
+++ src/main/java/udemy/edu/section2/UpdatedDropDown.java
@@ -0,0 +1,12 @@
+package udemy.edu.section2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropDown {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://spicejet.com");
+    }
+}

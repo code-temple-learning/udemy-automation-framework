fb4995d591894541e6f870d19b3f728a40cecbdc
anthonyhenderson <cuzixsin@gmail.com>
2024-10-21 13:08:43 -0700

    getting size of checkboxes Assertions in Automation

--- src/main/java/udemy/edu/section2/UpdatedDropDown.java
+++ src/main/java/udemy/edu/section2/UpdatedDropDown.java
@@ -3,18 +3,26 @@ package udemy.edu.section2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class UpdatedDropDown {
 
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("http://spicejet.com");
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        // System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+        System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
         driver.findElement(By.id("divpaxinfo")).click();
         Thread.sleep(2000L);
         for (int i = 1; i < 5; i++) {
             driver.findElement(By.id("hrefIncAdt")).click();
         }
         driver.findElement(By.id("btnlosepaxoption")).click();
+        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
         System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
     }
 }

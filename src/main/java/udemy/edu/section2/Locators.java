0b58415c0b75ed59a8786b611ebba363cf470772
anthonyhenderson <cuzixsin@gmail.com>
2024-10-22 10:38:33 -0700

    New username and password methods

--- src/main/java/udemy/edu/section2/Locators.java
+++ src/main/java/udemy/edu/section2/Locators.java
@@ -8,30 +8,29 @@ import java.time.Duration;
 
 public class Locators {
 
+    static WebDriver driver = new ChromeDriver();
+    static String[] userNames = {"rahul", "shetty"};
+    static String[] passwordList = {"rahulshettyacademy", "cuzi"};
     public static void main(String[] args) throws InterruptedException {
-        String name = "rahul";
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        String password = getPassword(driver);
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
-        driver.findElement(By.id("inputUsername")).sendKeys(name);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String password = getPassword(0);
+        String username = getUserName(0);
+        driver.findElement(By.id("inputUsername")).sendKeys(username);
         driver.findElement(By.name("inputPassword")).sendKeys(password);
         driver.findElement(By.className("signInBtn")).click();
         Thread.sleep(2000);
         System.out.println(driver.findElement(By.tagName("p")).getText());
         Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
-        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + username + ",");
         driver.findElement(By.xpath("//*[text()='Log Out']")).click();
         driver.close();
     }
-    public static String getPassword(WebDriver driver) throws InterruptedException {
-        driver.get("https://rahulshettyacademy.com/locatorspractice/");
-        driver.findElement(By.linkText("Forgot your password?")).click();
-        Thread.sleep(1000);
-        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
-        String[] passwordArray = passwordText.split("'");
-        String password = passwordArray[1].split("'")[0];
-        return password;
+    public static String getPassword(int choosePassword) {
+        return passwordList[choosePassword];
     }
 
+    public static String getUserName(int chooseUser) {
+        return userNames[chooseUser];
+    }
 }

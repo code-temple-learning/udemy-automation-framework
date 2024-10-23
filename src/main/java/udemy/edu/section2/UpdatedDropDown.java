27d9de9622e24c53f2a5e63592531e95bd4d21ba
anthonyhenderson <cuzixsin@gmail.com>
2024-10-23 10:54:13 -0700

    Validating if UI elements are enabled or disabled

--- src/main/java/udemy/edu/section2/UpdatedDropDown.java
+++ src/main/java/udemy/edu/section2/UpdatedDropDown.java
@@ -16,14 +16,24 @@ public class UpdatedDropDown {
         driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
         System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
         Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
-        System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
-        driver.findElement(By.id("divpaxinfo")).click();
-        Thread.sleep(2000L);
-        for (int i = 1; i < 5; i++) {
-            driver.findElement(By.id("hrefIncAdt")).click();
+        // System.out.println(driver.findElement(By.name("ctl00$mainContent$view_date2")).isEnabled());
+        System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+        driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+        System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+        if (driver.findElement(By.id("Div1")).getAttribute("style").contains("1")) {
+            System.out.println("It's enabled");
+            Assert.assertTrue(true);
+        } else {
+            Assert.assertTrue(false);
         }
-        driver.findElement(By.id("btnlosepaxoption")).click();
-        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
-        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+//        System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+//        driver.findElement(By.id("divpaxinfo")).click();
+//        Thread.sleep(2000L);
+//        for (int i = 1; i < 5; i++) {
+//            driver.findElement(By.id("hrefIncAdt")).click();
+//        }
+//        driver.findElement(By.id("btnlosepaxoption")).click();
+//        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+//        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
     }
 }

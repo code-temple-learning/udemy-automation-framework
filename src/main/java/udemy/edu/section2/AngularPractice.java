3da2a0fa6bb09393367e8ccad9b665b3d11cd120
anthonyhenderson <cuzixsin@gmail.com>
2024-10-25 12:26:24 -0700

    addressed PR comments

--- src/main/java/udemy/edu/section2/AngularPractice.java
+++ src/main/java/udemy/edu/section2/AngularPractice.java
@@ -15,8 +15,8 @@ public class AngularPractice {
         driver.findElement(By.name("email")).sendKeys("xsin@sexmail.org");
         driver.findElement(By.id("exampleInputPassword1")).sendKeys("greyFox");
         driver.findElement(By.id("exampleCheck1")).click();
-        WebElement staticDropdown = driver.findElement(By.id("exampleFormControlSelect1"));
-        Select dropdown = new Select(staticDropdown);
+        WebElement options = driver.findElement(By.id("exampleFormControlSelect1"));
+        Select dropdown = new Select(options);
         dropdown.selectByIndex(1);
         driver.findElement(By.id("inlineRadio1")).click();
         driver.findElement(By.name("bday")).sendKeys("02/25/1994");
--- src/main/java/udemy/edu/section2/SeleniumGroceryStore.java
+++ src/main/java/udemy/edu/section2/SeleniumGroceryStore.java
@@ -4,17 +4,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumGroceryStore {
 
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // explicit wait can catch performance issues, but it takes more code to use
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); implicit wait is cleaner overall for the code, but cannot catch performance issues
         String[] items = {"Cucumbers", "Broccoli"};
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
-        Thread.sleep(3000);
+        addItems(driver, items);
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.cssSelector("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+        driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button.promoBtn")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+        System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+
+        driver.close();
+        System.out.println("TEST SUCCESS");
+    }
+
+    public static void addItems(WebDriver driver, String[] items) {
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
         for (int i = 0; i < products.size(); i++) {
             String[] name = products.get(i).getText().split("-");
@@ -29,7 +47,5 @@ public class SeleniumGroceryStore {
                 }
             }
         }
-        driver.close();
-        System.out.printf("TEST SUCCESS");
     }
 }

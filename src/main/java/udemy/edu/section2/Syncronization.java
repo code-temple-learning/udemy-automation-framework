775f4e4fb230052e46ab3aaa7b845ca64eaa3353
anthonyhenderson <cuzixsin@gmail.com>
2024-10-25 12:27:44 -0700

    Working on assignment

---
+++ src/main/java/udemy/edu/section2/Syncronization.java
@@ -0,0 +1,28 @@
+package udemy.edu.section2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Syncronization {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+        driver.findElement(By.id("username")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("password")).sendKeys("learning");
+        driver.findElement(By.cssSelector(".customradio:nth-child(2)")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("okayBtn")));
+        driver.findElement(By.id("okayBtn")).click();
+//        WebElement options = driver.findElement(By.cssSelector(select.form-control));
+//        Select dropdown = new Select(options);
+//        dropdown.selectByValue("consult");
+    }
+}

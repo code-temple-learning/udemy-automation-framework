dd6c1c7d909dcf04915262d57492f795d851b955
anthonyhenderson <cuzixsin@gmail.com>
2024-11-05 13:20:25 -0800

    fixed PersonalLoan Test parameters

--- src/test/java/browsertests/day2.java
+++ src/test/java/browsertests/day2.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class day2 {
 
-    @Parameters ({"URL, APIKey/username"})
+    @Parameters ({"URL", "APIKey/username"})
     @Test
     public void PersonalLoan(String webName, String key) {
         System.out.println("good");
